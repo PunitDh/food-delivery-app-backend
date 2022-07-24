101d9da4b7e5026f0dc4c96e92172ddf3aacf857
PrateekKhindri <prateek.khindri90@gmail.com>
2022-07-24 17:56:43 +1000

    FDA-025 - Created carts controller [Prateek]

---
+++ src/controllers/CartsController.js
@@ -0,0 +1,74 @@
+const db = require("../config/dbConfig");
+
+const Carts = {
+  all: async () => {
+    const data = await db.query("SELECT * FROM carts;");
+    return data.rows;
+  },
+
+  findOne: async (id) => {
+    const data = await db.query("SELECT * FROM carts WHERE id=$1;", [id]);
+    return data.rows;
+  },
+
+  createOne: async ({ userid }) => {
+    const data = await db.query(
+      "INSERT INTO carts (userid) VALUES ($1) RETURNING *;",
+      [userid]
+    );
+    return data.rows;
+  },
+
+  updateOne: async ({ id, userid }) => {
+    const data = await db.query(
+      "UPDATE carts SET userid=$1 WHERE id=$2 RETURNING *;",
+      [userid, id]
+    );
+    return data.rows;
+  },
+
+  deleteOne: async (id) => {
+    const data = await db.query("DELETE FROM carts WHERE id=$1;", [id]);
+    return data.rows;
+  },
+};
+
+module.exports = Carts;
+
+// (async () => {
+//   console.log(await Users.all());
+// })();
+
+// (async () => {
+//   console.log(await Users.findOne(1));
+// })();
+
+// (async () => {
+//   console.log(await Users.deleteOne(1));
+// })();
+
+// (async () => {
+//   console.log(
+//     await Users.createOne({
+//       firstname: "Prateek",
+//       lastname: "Smith",
+//       email: "me@me.com",
+//       password: "postgresisnotawsum",
+//       admin: true,
+//     })
+//   );
+// })();
+
+//   (async () => {
+//     console.log(
+//       await Users.updateOne({
+//         id: 3,
+//         firstname: "Punit",
+//         lastname: "Jackson",
+//         email: "me@me.com",
+//         password: "Chinki",
+//         admin: true,
+//       })
+//     );
+//   }
+// )();
