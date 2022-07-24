6b378dda8ca44d9f0846e22daf204b3095d0a7ff
PrateekKhindri <prateek.khindri90@gmail.com>
2022-07-24 18:08:58 +1000

    FDA-025 - Created cart items controller [Prateek]

---
+++ src/controllers/CartItemsController.js
@@ -0,0 +1,74 @@
+const db = require("../config/dbConfig");
+
+const CartItems = {
+  all: async () => {
+    const data = await db.query("SELECT * FROM cartitems;");
+    return data.rows;
+  },
+
+  findOne: async (id) => {
+    const data = await db.query("SELECT * FROM cartitems WHERE id=$1;", [id]);
+    return data.rows;
+  },
+
+  createOne: async ({ cartid, itemid, quantity }) => {
+    const data = await db.query(
+      "INSERT INTO cartitems (cartid, itemid, quantity) VALUES ($1, $2, $3) RETURNING *;",
+      [cartid, itemid, quantity]
+    );
+    return data.rows;
+  },
+
+  updateOne: async ({ id, cartid, itemid, quantity }) => {
+    const data = await db.query(
+      "UPDATE cartitems SET cartid=$1, itemid=$2, quantity=$3 WHERE id=$4 RETURNING *;",
+      [cartid, itemid, quantity, id]
+    );
+    return data.rows;
+  },
+
+  deleteOne: async (id) => {
+    const data = await db.query("DELETE FROM cartitems WHERE id=$1;", [id]);
+    return data.rows;
+  },
+};
+
+module.exports = CartItems;
+
+// (async () => {
+//   console.log(await Users.all());
+// })();
+
+// (async () => {
+//   console.log(await Users.findOne(1));
+// })();
+
+// (async () => {
+//   console.log(await Users.deleteOne(1));
+// })();
+
+// (async () => {
+//   console.log(
+//     await Users.createOne({
+//       firstname: "Prateek",
+//       lastname: "Smith",
+//       email: "me@me.com",
+//       password: "postgresisnotawsum",
+//       admin: true,
+//     })
+//   );
+// })();
+
+//   (async () => {
+//     console.log(
+//       await Users.updateOne({
+//         id: 3,
+//         firstname: "Punit",
+//         lastname: "Jackson",
+//         email: "me@me.com",
+//         password: "Chinki",
+//         admin: true,
+//       })
+//     );
+//   }
+// )();
