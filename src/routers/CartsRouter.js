a469c03b73351068b5134b3b32023f38f853127a
PrateekKhindri <prateek.khindri90@gmail.com>
2022-10-08 15:22:53 +1100

    FDA-007- Added remove items from cart API [Prateek]

--- src/routers/CartsRouter.js
+++ src/routers/CartsRouter.js
@@ -1,4 +1,5 @@
 const CartItems = require("../controllers/CartItemsController");
+const Items = require("../controllers/ItemsController");
 
 const router = require("express").Router();
 
@@ -7,25 +8,52 @@ router.post("/:cartid/add/:itemid", async (req, res) => {
   const itemInCart = await CartItems.findItemInCart({ cartid, itemid });
 
   if (itemInCart.length > 0) {
-    const cartItem = await CartItems.updateOne({
-      id: itemInCart[0].id,
+    const items = await CartItems.findAndUpdateQuantity({
       cartid,
       itemid,
-      quantity: itemInCart[0].quantity + 1,
+      quantityToUpdate: 1,
     });
-    console.log(cartItem);
+
+    res.status(200).send(items);
   } else {
-    const cartItem = await CartItems.createOne({
+    const result = await CartItems.createOne({
       cartid,
       itemid,
       quantity: 1,
     });
-    console.log(cartItem);
+
+    res.status(201).send(result);
   }
 
-  const itemsInCurrentCart = await CartItems.findItemsByCartId(cartid);
+  // const itemsInCurrentCart = await CartItems.findItemsByCartId(cartid);
+});
 
-  res.send(itemsInCurrentCart);
+router.get("/items/:cartid", async (req, res) => {
+  const itemsInCurrentCart = await CartItems.findItemsByCartId(
+    req.params.cartid
+  );
+
+  const items = [];
+  for (const cartItem of itemsInCurrentCart) {
+    const item = await Items.findOne(cartItem.itemid);
+
+    items.push({ quantity: cartItem.quantity, ...item });
+  }
+
+  res.send(items);
+});
+
+router.delete("/:cartid/remove/:itemid", async (req, res) => {
+  const { cartid, itemid } = req.params;
+  const itemToRemove = await CartItems.findAndUpdateQuantity({
+    cartid,
+    itemid,
+    quantityToUpdate: -1,
+  });
+
+  // const itemsInCurrentCart = await CartItems.findItemsByCartId(cartid);
+
+  res.send(itemToRemove);
 });
 
 module.exports = router;
